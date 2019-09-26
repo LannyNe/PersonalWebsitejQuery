e6bc26736c0a99cf144815b03fc9ed5f96efa098
LannyNe <54725614+LannyNe@users.noreply.github.com>
2019-09-26 16:03:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $("#background").click(
+    function(){
+        $("#bgd").slideToggle();
+    }),
+    $("#future").click(
+    function(){
+        $("#ftr").fadeToggle();
+    });
+    $("#background").hover(
+    function(){
+        $(this).html("<h3>Click to collapse.</h3>");
+    },
+    function(){
+        $(this).html("<h3>My Background</h3>");
+    })
+
+// change background depending on user's time
+var datetoday = new Date();
+var thehour = datetoday.getHours();
+if (thehour > 18 && thehour < 25) {
+$('*').addClass('n');}
+else if (thehour > 5 && thehour < 19) {
+$('*').addClass('d');}
+else {
+$('*').addClass('n');};
+
+// alert on image
+$("#image").mouseenter(
+       function(){
+       alert("Me at my cousin's wedding in 2010");
+    });
+});
